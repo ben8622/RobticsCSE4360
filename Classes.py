7e78db6ddba45f53aa4a260906cacc482404d7f5
Ben Knight <benjaminjknight0987@gmail.com>
2021-10-28 21:23:29 -0500

    map with obstacles and start/endgoal

--- Classes.py
+++ Classes.py
@@ -6,6 +6,8 @@ class Node:
     self.y = 0.0
     self.is_obstacle = False
     self.visited = False
+    self.is_start = False
+    self.is_goal = False
 
   def set_xy(self, x, y):
     self.x = x
@@ -14,8 +16,14 @@ class Node:
   def set_visited(self):
     self.visited = True
 
-  def set_is_object(self):
-    self.is_object = True
+  def set_is_obstacle(self):
+    self.is_obstacle = True
+
+  def set_is_start(self):
+    self.is_start = True
+
+  def set_is_goal(self):
+    self.is_goal = True
 
   def build_empty_map(self):
     empty_map = []
@@ -39,16 +47,52 @@ class Map:
         empty_row.append(node)
       self.map.append(empty_row)
       
-  def print_map(self):
+  def print_map_coords(self):
     for i in range(constant.ROWS):
       for j in range(constant.COLS):
         print(f'({self.map[i][j].x},{self.map[i][j].y})', end="")
       print("")
 
-  def add_obstacles(self):
-    for obstacle in constant.OBSTACLES:
+  def print_obstacle_map(self):
+    for i in range(constant.ROWS):
+      for j in range(constant.COLS):
+        is_obstacle = 1 if self.map[i][j].is_obstacle else 0
+        print(f'(   {is_obstacle}   )', end="")
+      print("")
+
+  def print_start_and_goal_map(self):
+    for i in range(constant.ROWS):
+      for j in range(constant.COLS):
+        temp = 1 if self.map[i][j].is_start or self.map[i][j].is_goal else 0
+        print(f'(   {temp}   )', end="")
+      print("")
+
+  def print_complete_map(self):
+    for i in range(constant.ROWS):
+      for j in range(constant.COLS):
+        temp = ' '
+        if(self.map[i][j].is_obstacle): temp = 'O'
+        elif(self.map[i][j].is_start): temp = 'S'
+        elif(self.map[i][j].is_goal): temp = 'G'
+        print(f'[ {temp} ]', end="")
+      print("")
+
+  def set_obstacles(self):
+    for obstacle in constant.TEST_OBSTACLES:
       if obstacle[0] == -1: continue
       obs_x = round(obstacle[0] * constant.METERS_TO_FEET, 2)
       obs_y = round(obstacle[1] * constant.METERS_TO_FEET, 2)
-      print(f'({obstacle[0]},{obstacle[1]})', end=" | ")
-      print(f'({obs_x},{obs_y})')
+      for row in self.map:
+        for node in row:
+          if(node.x == obs_x and node.y == obs_y):
+            node.set_is_obstacle()
+
+  def set_start_and_goal(self):
+    start = ( round(constant.START[0] * constant.METERS_TO_FEET, 2), round(constant.START[1] * constant.METERS_TO_FEET, 2) )
+    goal = ( round(constant.GOAL[0] * constant.METERS_TO_FEET, 2), round(constant.GOAL[1] * constant.METERS_TO_FEET, 2) )
+    for row in self.map:
+      for node in row:
+        if((node.x, node.y) == start):
+          node.set_is_start()
+        elif((node.x, node.y) == goal):
+          node.set_is_goal()
