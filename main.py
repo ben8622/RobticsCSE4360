c009355b9513f4502ba8411fb3520db239991176
Ben Knight <benjaminjknight0987@gmail.com>
2021-11-02 19:32:12 -0500

    final version, also updated pdf

--- main.py
+++ main.py
@@ -12,20 +12,17 @@ ev3 = EV3Brick()
 left_motor = Motor(Port.A)
 right_motor = Motor(Port.D)
 ev3.speaker.beep(500, 450)
-offset = 4
+offset = 1
 
-def turn_right():
-  left_motor.run_time(-constant.TURN_SPEED, constant.TURN_TIME, Stop.HOLD, False)
-  right_motor.run_time(constant.TURN_SPEED-offset, constant.TURN_TIME, Stop.HOLD, True)
 def turn_left():
-  left_motor.run_time(constant.TURN_SPEED, constant.TURN_TIME, Stop.HOLD, False)
-  right_motor.run_time(-constant.TURN_SPEED+offset, constant.TURN_TIME, Stop.HOLD, True)
+  left_motor.run_time(-constant.TURN_SPEED-offset, constant.TURN_TIME, Stop.HOLD, False)
+  right_motor.run_time(constant.TURN_SPEED, constant.TURN_TIME, Stop.HOLD, True)
+def turn_right():
+  left_motor.run_time(constant.TURN_SPEED+offset, constant.TURN_TIME, Stop.HOLD, False)
+  right_motor.run_time(-constant.TURN_SPEED, constant.TURN_TIME, Stop.HOLD, True)
 def move_forward():
-  left_motor.run_time(constant.MOVE_SPEED, constant.MOVE_TIME, Stop.HOLD, False)
-  right_motor.run_time(constant.MOVE_SPEED-offset, constant.MOVE_TIME, Stop.HOLD, True)
-def move_backward(ms):
-  left_motor.run_time(-250, ms, Stop.HOLD, False)
-  right_motor.run_time(-250, ms, Stop.HOLD, True)
+  left_motor.run_time(constant.MOVE_SPEED-offset, constant.MOVE_TIME, Stop.HOLD, False)
+  right_motor.run_time(constant.MOVE_SPEED, constant.MOVE_TIME, Stop.HOLD, True)
 
 def perform_commands(commands):
   for command in commands:
@@ -41,18 +38,36 @@ map.set_man_values()
 # Sound to signify start
 ev3.speaker.beep()
 
-move_forward()
-time.sleep(1)
+# move_forward()
+# move_forward()
+# move_forward()
+# move_forward()
+# move_forward()
+# move_forward()
+# move_forward()
+# move_forward()
+# move_forward()
+# move_forward()
+# time.sleep(1)
 
-turn_right()
-time.sleep(1)
+# turn_right()
+# time.sleep(1)
 
-turn_left()
-time.sleep(1)
+# turn_left()
+# time.sleep(1)
 
-# while(not map.visited_goal()):
-#   commands = map.next_node()
-#   perform_commands(commands)
+# turn_right()
+# time.sleep(1)
+
+# turn_left()
+# time.sleep(1)
+
+while(not map.visited_goal()):
+  commands = map.next_node()
+  perform_commands(commands)
+
+# turn_left();
+# turn_right();
 
 # Play another beep sound.
 ev3.speaker.beep(1000, 500)
