58672356aabd3c108796b7bc34b7f1a7bf060a49
Ben Knight <benjaminjknight0987@gmail.com>
2021-11-04 15:48:45 -0500

    some major cleanup

--- Project_1/pathfinding/main.py
+++ Project_1/pathfinding/main.py
@@ -8,10 +8,15 @@ import Classes
 
 # Initialize the EV3 brick.
 ev3 = EV3Brick()
-# Initialize a motor at port B.
+
+# Initialize a motor at port A and D.
 left_motor = Motor(Port.A)
 right_motor = Motor(Port.D)
+
+# Beep to notify ev3 is initialized
 ev3.speaker.beep(500, 450)
+
+## IMPORTANT, this offset is used to try and correct the difference in our right motor speed, remove for your own robot
 offset = 1
 
 def turn_left():
@@ -30,44 +35,18 @@ def perform_commands(commands):
     elif(command == "turn_left"): turn_left()
     elif(command == "move"): move_forward()
 
+## Build map
 map = Classes.Map()
 map.set_obstacles()
 map.set_start_and_goal()
 map.set_man_values()
 
-# Sound to signify start
+# Sound to signify map is done building
 ev3.speaker.beep()
-
-# move_forward()
-# move_forward()
-# move_forward()
-# move_forward()
-# move_forward()
-# move_forward()
-# move_forward()
-# move_forward()
-# move_forward()
-# move_forward()
-# time.sleep(1)
-
-# turn_right()
-# time.sleep(1)
-
-# turn_left()
-# time.sleep(1)
-
-# turn_right()
-# time.sleep(1)
-
-# turn_left()
-# time.sleep(1)
 
 while(not map.visited_goal()):
   commands = map.next_node()
   perform_commands(commands)
 
-# turn_left();
-# turn_right();
-
-# Play another beep sound.
+# Play another beep when finished moving
 ev3.speaker.beep(1000, 500)
---
+++ Project_1/pathfinding/testing.py
@@ -0,0 +1,19 @@
+import Classes
+import constant
+
+## init Map
+map = Classes.Map()
+map.set_obstacles()
+map.set_start_and_goal()
+map.set_man_values()
+
+## Display complete map (shows obstacles, start, and goal)
+map.print_complete_map()
+
+## Do the "local" path finding
+while(not map.visited_goal()):
+  commands = map.next_node()
+  print(commands)
+
+## Print nodes that have been visited
+map.print_visited_map()
