1a8f639f580426f152726eec9f6b432b828e59ba
Roger Chapa <rogerchapa68@gmail.com>
2021-11-28 17:51:27 -0600

    turn right wheel on corners
    also added ultra sonic sensor and inch conversion constant

--- Project_2/main.py
+++ Project_2/main.py
@@ -20,6 +20,7 @@ ev3 = EV3Brick()
 # Initialize Sensors & Motors
 nxt_sensor = LightSensor(Port.S1)
 ev3_sensor = ColorSensor(Port.S4)
+us_sensor = UltrasonicSensor(Port.S2)
 left_motor = Motor(Port.D)
 right_motor = Motor(Port.C)
 
@@ -32,8 +33,8 @@ going_straight = 0
 def wander():
   global wander_counter
   global going_straight
-  left_motor.run(-400 + wander_counter)
-  right_motor.run(-250)
+  left_motor.run(-200 )
+  right_motor.run(-50 - wander_counter)
 
   # Radius of our circle gets biger until
   # we are driving straight, then we drive
@@ -51,37 +52,46 @@ def stop():
   left_motor.hold()
   right_motor.hold()
 
+def found_wall():
+  nxt_reflection = nxt_sensor.reflection()
+  ev3_reflection = ev3_sensor.reflection()
+  return (nxt_reflection  <= const.NXT_DETECT)
+
 def follow_wall():
-  if(nxt_reflection <= const.NXT_DETECT):
+  if(found_wall()):
     right_motor.stop()
     left_motor.run(-100)
+
+    if(found_wall()):
+      while found_wall():
+        right_motor.run(100)
+      right_motor.stop()
+
   else:
     left_motor.stop()
     right_motor.run(-100)
 
-
 ev3.speaker.say("Starting program")
 
+wait_time = 300 #not using this yet
+goal_reached = False
 wall_found = False
+inches_const = 25.4
 
-while not wall_found:
-  nxt_reflection = nxt_sensor.reflection()
-  ev3_reflection = ev3_sensor.reflection()
+#this gives distance in inches
+#us_sensor.distance() / 25.4
 
-  if(nxt_reflection  <= const.NXT_DETECT):
+while not goal_reached:
+  if(found_wall()):
     stop()
     ev3.speaker.say("Wall Found")
     wall_found = True
+
+    while wall_found:
+      follow_wall()
+    
   else:
     wander()
 
-  wait(5)
+  wait(10)
 
-#wall follwing func
-while wall_found:
-  nxt_reflection = nxt_sensor.reflection()
-  ev3_reflection = ev3_sensor.reflection()
-
-  follow_wall(nxt_reflection, ev3_reflection)
-
-  wait(5)
