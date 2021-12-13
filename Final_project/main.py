421acababe242819fc75ce6028a10abd055a10f1
Roger Chapa <rogerchapa68@gmail.com>
2021-12-12 18:29:34 -0600

    initiallized sensors and tested

--- Final_project/main.py
+++ Final_project/main.py
@@ -6,6 +6,9 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+from pybricks.tools import wait
+from pybricks.nxtdevices import LightSensor
+
 
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
@@ -14,17 +17,38 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 
 # Create your objects here.
 ev3 = EV3Brick()
-elevator = Motor(Port.A)
+elevator = Motor(Port.C)
+touch_sens = TouchSensor(Port.S2)
+left_motor = Motor(Port.D)
+right_motor = Motor(Port.A)
+color_sens = ColorSensor(Port.S1)
+light_sens = LightSensor(Port.S4)
+gyro_sens = GyroSensor(Port.S3, Direction.CLOCKWISE)
 
 
 # Write your program here.
 ev3.speaker.beep()
 
 while(True):
+  # print(color_sens.color())
+  # print(gyro_sens.angle())
+  # print(light_sens.reflection())
+
+
+  if(touch_sens.pressed()):
+    print("pressed!")
+    elevator.run_time(-200, 5000, Stop.HOLD, True)
+    # left_motor.run(100)
+    # right_motor.run(100)
+    # wait(1000)
+    # left_motor.stop()
+    # right_motor.stop()
+
   if(ev3.buttons.pressed() == [Button.UP]):
-    elevator.run_time(500, 5000, Stop.HOLD, True)
+    elevator.run_time(1000, 5000, Stop.HOLD, True)
+
   elif(ev3.buttons.pressed() == [Button.DOWN]):
-    elevator.run_time(-500, 5000, Stop.HOLD, True)
+    elevator.run_time(-1000, 5000, Stop.HOLD, True)
 
   ev3.screen.print(ev3.buttons.pressed())
   
