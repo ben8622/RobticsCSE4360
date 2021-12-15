7406b32fe92a5fe11c6ba9a5123270d6a0a6bd15
Ben Knight <benjaminjknight0987@gmail.com>
2021-12-14 18:32:48 -0600

    idk

--- Final_project/main.py
+++ Final_project/main.py
@@ -21,55 +21,70 @@ elevator = Motor(Port.C)
 touch_sens = TouchSensor(Port.S2)
 left_motor = Motor(Port.D)
 right_motor = Motor(Port.A)
+rear_motor = Motor(Port.B)
 color_sens = ColorSensor(Port.S1)
 light_sens = LightSensor(Port.S4)
-gyro_sens = GyroSensor(Port.S3, Direction.COUNTERCLOCKWISE)
+gyro_sens = GyroSensor(Port.S3, Direction.CLOCKWISE)
 
 
 # Methods
+def is_pressed():
+  return touch_sens.pressed()
+
+def rear_motor_forward():
+  rear_motor.run(-100)
+
 def move_forward():
   left_motor.run(100)
   right_motor.run(100)
+  rear_motor_forward()
 
-def stop_motors():
+def stop_all_motors():
   left_motor.stop()
   right_motor.stop()
+  rear_motor.stop()
+  elevator.stop()
 
 def read_gyro():
   return gyro_sens.angle()
 
 def elevator_up():
-  elevator.run(-200)
-  wait(2000)
+  elevator.run(-300)
+  wait(4000)
   elevator.stop()
 
 def elevator_down():
   elevator.run(200)
-  wait(2000)
+  wait(9000)
   elevator.stop()
+
+
+def zero_elevator():
+  elevator.run(-200)
+  while(not is_pressed()):
+    continue
+  elevator.stop()
+  elevator.run_time(200, 9000, Stop.HOLD, True)
+  gyro_sens.reset_angle(0)
+
   
 # Write your program here.
 ev3.speaker.beep()
 
+zero_elevator()
+
+elevator_moving = False
 
 while(True):
-  # print(color_sens.color())
-  print(gyro_sens.angle())
-  # print(light_sens.reflection())
+  # if(ev3.buttons.pressed() == [Button.UP]):
+  #   elevator.run_time(100, 500, Stop.HOLD, True)
+  # if(ev3.buttons.pressed() == [Button.DOWN]):
+  #   elevator.run_time(-100, 500, Stop.HOLD, True)
+  if(read_gyro() > 20 and not elevator_moving):
+    ev3.speaker.beep()
+    elevator_up()
+    elevator_down()
+    # zero_elevator()
 
   move_forward()
 
-  # if(read_gyro() > 0):
-  #   elevator_up()
-
-  if(read_gyro() <= 0):
-    gyro_sens.reset_angle(0)
-    #elevator_down()  
-
-    # left_motor.run(100)
-    # right_motor.run(100)
-    # wait(1000)
-    # left_motor.stop()
-    # right_motor.stop()
-
-  ev3.screen.print(gyro_sens.angle())
